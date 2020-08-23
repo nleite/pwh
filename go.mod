b797374b5f704e481b62c5245771745839754e0b
nleite <git@norbertoleite.com>
2020-08-23 19:43:04 -0400

    obfuscating all the passwords
    first pass of this is done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nleite/pwh
 
 go 1.13
+
+require github.com/ttacon/uri v0.0.0-20151031145549-8a81e2049497 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,85 @@
 package main
 
 import "fmt"
+import "os"
+import "bufio"
+import "io"
+import "strings"
+import "regexp"
 
 func main() {
-	fmt.Println("pwh")
+	info, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
+	if info.Mode()&os.ModeCharDevice != 0 || info.Size() <= 0 {
+		fmt.Println("not using pipe")
+		return
+	}
+
+	processPipe()
+}
+
+func processPipe() {
+	reader := bufio.NewReader(os.Stdin)
+
+	var out []string
+
+	for {
+		// Probably need to see how to make it work for windows.
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+			return
+		}
+		//TODO: replace second attribute with flag defined value
+		processLine(line, "*")
+		out = append(out, string(line))
+	}
+}
+
+// processLine checks if there are any passwords in the line argument, and
+// replaces its characters with obf string.
+func processLine(line string, obf string) {
+
+	// check if there are any uri matching
+	line, found := matchURI(line, obf)
+	if !found {
+		line, _ = matchConfig(line, obf)
+	}
+	fmt.Printf("%s", line)
+}
+
+// Apply regexp URI expression - looks for a hint of uri placeholder.
+func matchURI(line string, obf string) (string, bool) {
+	r := regexp.MustCompile(`(?P<uri>://(?P<userinfo>[-\w;/?:]+)@)`)
+	matched := r.FindStringSubmatch(line)
+	if len(matched) < 3 {
+		return line, false
+	}
+	userInfo := matched[2]
+	parts := strings.Split(userInfo, ":")
+	if len(parts) > 1 {
+		replacer := strings.Repeat(obf, len(parts[1]))
+		return strings.Replace(line, parts[1], replacer, 1), true
+	}
+	return line, false
+}
+
+// Looks out to match passwords defined in config files
+func matchConfig(line string, obf string) (string, bool) {
+	r := regexp.MustCompile(`^((?i).*PASS?.+(:|=))([ A-z0-9"']+)`)
+	matched := r.FindStringSubmatch(line)
+	if len(matched) < 4 {
+		return line, false
+	}
+	pwd := r.FindStringSubmatch(line)[3]
+	if len(pwd) > 0 {
+		replacer := strings.Repeat(obf, len(pwd))
+		return strings.Replace(line, pwd, replacer, 1), true
+	}
+	return line, false
 }
